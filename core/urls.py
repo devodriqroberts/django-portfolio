e5e27cf4d07c92b690d3acac41ebba354fd957fa
Devodriq Roberts <Devorob26@gmail.com>
2019-08-16 15:49:33 -0400

    Added redirect path

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomeTemplateView, AboutTemplateView
+from .views import HomeTemplateView, redirect_portfolio
 
 urlpatterns = [
+    path('', redirect_portfolio),
     path('portfolio', HomeTemplateView.as_view()),
     # path('about-me', AboutTemplateView.as_view()),
 ]
