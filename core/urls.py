a6f0614124b33d6c8da4f7b5818c7a8e1d1292d4
Devodriq Roberts <Devorb26@gmail.com>
2019-08-16 00:34:53 -0400

    Modified portfolio path

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import HomeTemplateView, AboutTemplateView
 
 urlpatterns = [
-    path('', HomeTemplateView.as_view()),
+    path('portfolio', HomeTemplateView.as_view()),
     # path('about-me', AboutTemplateView.as_view()),
 ]
